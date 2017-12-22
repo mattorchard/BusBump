d89ed10bc82beebfc2bfb9f7ad1b5b8102aa8143
morch028 <morch028@uottaw.ca>
2017-12-22 12:48:35 -0500

    Add boilerplate RecyclerView content
    Adds placeholder content for RecyclerView Adapter, ViewHolder, Layout.

--- app/src/main/java/com/orchard/seg/busbump/activity/MainActivity.java
+++ app/src/main/java/com/orchard/seg/busbump/activity/MainActivity.java
@@ -4,14 +4,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.orchard.seg.busbump.R;
+import com.orchard.seg.busbump.adapter.BusInfoAdapter;
+import com.orchard.seg.busbump.model.BusInfo;
+import com.orchard.seg.busbump.viewholder.BusInfoViewHolder;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<BusInfo> mDataSet;
+
+    private RecyclerView mBusInfoRv;
+    private RecyclerView.LayoutManager mBusInfoLayoutManager;
+    private RecyclerView.Adapter<BusInfoViewHolder> mBusInfoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Todo: Replace placeholder data with call to ORM for placeholder data.
+        mDataSet = getSampleDataSet();
+
+        mBusInfoRv = (RecyclerView) findViewById(R.id.rv_main_bus_list);
+        mBusInfoLayoutManager = new LinearLayoutManager(MainActivity.this);
+        mBusInfoRv.setLayoutManager(mBusInfoLayoutManager);
+        mBusInfoAdapter = new BusInfoAdapter(mDataSet);
+        mBusInfoRv.setAdapter(mBusInfoAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,5 +73,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Todo: Remove this method and replace with ORM.
+    private List<BusInfo> getSampleDataSet() {
+        List<BusInfo> dataSet = new LinkedList<>();
+        dataSet.add(new BusInfo(7613, 16));
+        dataSet.add(new BusInfo(7610, 16));
+        dataSet.add(new BusInfo(3020, 97, 1));
+        return dataSet;
     }
 }
--- app/src/main/java/com/orchard/seg/busbump/viewholder/BusInfoViewHolder.java
+++ app/src/main/java/com/orchard/seg/busbump/viewholder/BusInfoViewHolder.java
@@ -1,4 +1,29 @@
 package com.orchard.seg.busbump.viewholder;
 
-public class BusInfoViewHolder {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.orchard.seg.busbump.R;
+import com.orchard.seg.busbump.model.BusInfo;
+
+import java.util.Locale;
+
+public class BusInfoViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView mPlaceholderTv;
+
+    public BusInfoViewHolder(View itemView) {
+        super(itemView);
+        this.mPlaceholderTv = (TextView) itemView.findViewById(R.id.tv_item_bus_info_placeholder);
+    }
+
+    public void bindView(BusInfo busInfo) {
+        String placeholderText = String.format(Locale.getDefault(),
+                "%d|%d|%d",
+                busInfo.getStopNumber(),
+                busInfo.getBusNumber(),
+                busInfo.getDirectionNumber());
+        mPlaceholderTv.setText(placeholderText);
+    }
 }
