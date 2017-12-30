b6347875a170088a4002b6b27120beb2bd41fc50
morch028 <morch028@uottaw.ca>
2017-12-29 19:56:20 -0500

    Add REST client
    Generic rest client, and OCTranspo rest client added. These serve
    to query the OCTranspo web service. It may become more appropriate
    later to create an OCTranspo client subclass which persists keys
    and allows for subclasses to attain them while performing more
    specialized actions.

---
+++ app/src/main/java/com/orchard/seg/busbump/network/RestClient.java
@@ -0,0 +1,61 @@
+package com.orchard.seg.busbump.network;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class RestClient {
+
+    private static final int TIMEOUT = 2000;
+    private String mUrlBase;
+
+    public RestClient(@NonNull String urlBase) {
+        this.mUrlBase = urlBase;
+        if (!mUrlBase.endsWith("/") && !mUrlBase.endsWith("\\")) {
+            mUrlBase += "/";
+        }
+    }
+
+    public String getRequest(String urlPath) throws IOException {
+        return getRequest(urlPath, HttpURLConnection.HTTP_OK);
+    }
+
+    public String getRequest(String urlPath, int expectedResponseCode) throws IOException {
+        String urlFull = mUrlBase + urlPath;
+        Log.d("FooBar", "Making request to: " + urlFull);
+        HttpURLConnection connection =(HttpURLConnection)new URL(urlFull).openConnection();
+        connection.setConnectTimeout(TIMEOUT);
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        int actualResponseCode = connection.getResponseCode();
+        if (expectedResponseCode != actualResponseCode) {
+            throw new IOException("Error from server: "
+                                    + actualResponseCode
+                                    + ". With Message: "
+                                    + connection.getResponseMessage());
+        }
+        return readResponse(connection.getInputStream());
+    }
+
+    private String readResponse(@NonNull InputStream stream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        try {
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+            return builder.toString();
+        } finally {
+            stream.close();
+        }
+    }
+}
